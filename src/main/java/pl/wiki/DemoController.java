b508fe517839ad0be909d0c15ec6158747f6451c
bkoz494 <bartux123@gmail.com>
2024-04-04 13:47:29 +0200

    basic app with Keycloak

---
+++ src/main/java/pl/wiki/DemoController.java
@@ -0,0 +1,19 @@
+package pl.wiki;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+
+    @GetMapping(path = "/")
+    public String welcomePage(){
+        return "welcome";
+    }
+
+    @GetMapping("/demo")
+    public String demoPage(){
+        return "demo";
+    }
+}
