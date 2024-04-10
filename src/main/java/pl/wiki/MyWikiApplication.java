e08823e4eecf9d62c6089b1eee8460fd8302986a
bkoz494 <bartux123@gmail.com>
2024-04-10 09:50:45 +0200

    add open API

--- src/main/java/pl/wiki/MyWikiApplication.java
+++ src/main/java/pl/wiki/MyWikiApplication.java
@@ -1,11 +1,14 @@
 package pl.wiki;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "MyWiki API", version = "1.0", description = "MyWiki endpoints"))
 public class MyWikiApplication {
 
 	public static void main(String[] args) {
